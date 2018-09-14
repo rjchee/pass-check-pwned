5b4b8f6e40ffc87f37c98846f8fc05b65e9decbe
Raymond Chee <rjchee@gmail.com>
2018-09-13 23:23:25 -0400

    Add verbose flag to inform user which password is currently being checked

--- src/check.bash
+++ src/check.bash
@@ -3,7 +3,7 @@
 set -o pipefail
 
 SCRIPT_VERSION="1.0.0"
-CHECK_USAGE_STRING="Usage: $PROGRAM $COMMAND [--help,-h] [--version] [--line=line-number,-l line-number] [--short-output,-s] pass-names"
+CHECK_USAGE_STRING="Usage: $PROGRAM $COMMAND [--help,-h] [--version] [--line=line-number,-l line-number] [--short-output,-s] [--verbose, -v] pass-names"
 
 check_usage () {
   echo "$CHECK_USAGE_STRING"
@@ -14,15 +14,16 @@ check_version () {
 }
 
 cmd_check () {
-  local short_output=0 selected_line=1
-  opts="$($GETOPT -o hl:s -l help,version,line:,short-output -n pass-check -- "$@")"
+  local short_output=false selected_line=1 verbose=false
+  opts="$($GETOPT -o hl:sv -l help,version,line:,short-output,verbose -n pass-check -- "$@")"
   [[ $? -eq 0 ]] || die "$CHECK_USAGE_STRING"
   eval set -- "${opts[@]}"
   while true; do case $1 in
     -h|--help) check_usage; return 0 ;;
     --version) check_version; return 0 ;;
     -l|--line) selected_line="$2"; shift 2 ;;
-    -s|--short-output) short_output=1; shift ;;
+    -s|--short-output) short_output=true; shift ;;
+    -v|--verbose) verbose=true; shift ;;
     --) shift; break ;;
   esac done
   [[ $selected_line =~ ^[0-9]+$ ]] || die "Line $selected_line is not a number."
@@ -34,22 +35,31 @@ cmd_check () {
 
   local all_clear=1
   for path in $targets; do
+    if $verbose; then
+      echo "Checking $path..." >&2
+    fi
     local pass sha_hash response
     pass=$(cmd_show $path | tail -n +${selected_line} | head -n 1)
     sha_hash=$(echo -n $pass | sha1sum | cut -d' ' -f1 | sed "s/.*/\U&/")
     response=$(curl -s -H "User-Agent: Pass-Password-Store-Checker/$SCRIPT_VERSION" "https://api.pwnedpasswords.com/range/${sha_hash:0:5}" | grep --color=never ${sha_hash:5})
     if [ $? -eq 0 ]; then
       local count=$(echo "$response" | cut -d':' -f2 | sed 's/.$//')
-      if [[ $short_output -eq 1 ]]; then
+      if $verbose; then
+        echo >&2
+      fi
+      if $short_output; then
         echo "$path:$count"
       else
         echo "$path has been pwned $count time(s)."
+      fi
+      if $verbose; then
+        echo >&2
       fi
       all_clear=0
     fi
   done
 
-  if [[ $short_output -ne 1 ]] && [[ $all_clear -eq 1 ]]; then
+  if ! $short_output && [[ $all_clear -eq 1 ]]; then
     echo "No pwned passwords found."
   fi
 }
